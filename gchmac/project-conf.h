8388718cc2345c01f804ef0284dbdc5c15538d7f
peppidesu <bakker.pepijn@gmail.com>
2025-10-16 20:06:18 +0200

    it sort of works

--- gchmac/project-conf.h
+++
@@ -1,6 +0,0 @@
-#define LOG_CONF_LEVEL_MAC                          LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_NULLNET                      LOG_LEVEL_INFO
-#define TSCH_SCHEDULE_CONF_WITH_6TISCH_MINIMAL 0
-#define TSCH_CONF_AUTOSELECT_TIME_SOURCE 1
-#define TSCH_CONF_WITH_LINK_SELECTOR 1
-#define TSCH_CALLBACK_PACKET_READY gchmac_callback_packet_ready
