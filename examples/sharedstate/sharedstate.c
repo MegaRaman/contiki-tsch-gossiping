a6613a4bfd9985400317bd5ac47881b2f116d1e4
Roman Spivachuk <roma.rio18121811@gmail.com>
2025-10-18 18:10:01 +0200

    Fix bugs

--- examples/sharedstate/sharedstate.c
+++ examples/sharedstate/sharedstate.c
@@ -19,21 +19,11 @@
 static sharedstate_t sharedstate;
 uint8_t msg_id = 0;
 
-inline int get_pkt_id(uint8_t pkt_id[PKT_ID_SIZE_BYTES])
-{
-	return pkt_id[1] << 8 | pkt_id[0];
-}
-
-inline int get_pkt_tstamp(uint8_t tstamp[TIMESTAMP_SIZE_BYTES])
-{
-	return tstamp[1] << 8 | tstamp[0];
-}
-
 // TODO: do we need a hashmap lookup?
 int inputbuf_contains(sharedstate_t *sharedstate, sharedstate_pkt_t *pkt)
 {
 	int pkt_id = get_pkt_id(pkt->pkt_id);
-	for (int i = 0; i < sharedstate->msgs_rx_nr; i++)
+	for (int i = 0; i < sharedstate->input_buf_cnt; i++)
 	{
 		if (get_pkt_id(sharedstate->input_buf[i].pkt_id) == pkt_id)
 		{
@@ -47,7 +37,7 @@ int inputbuf_contains(sharedstate_t *sharedstate, sharedstate_pkt_t *pkt)
 int cache_contains(sharedstate_t *sharedstate, sharedstate_pkt_t *pkt)
 {
 	int pkt_id = get_pkt_id(pkt->pkt_id);
-	for (int i = 0; i < sharedstate->cache_entries_nr; i++)
+	for (int i = 0; i < sharedstate->cache_entries_cnt; i++)
 	{
 		if (get_pkt_id(sharedstate->cache[i].pkt_id) == pkt_id)
 		{
@@ -65,29 +55,31 @@ void cache_remove(sharedstate_t *sharedstate, int cache_index)
 		return;
 	}
 	sharedstate->cache_occupied_index[cache_index] = false;
-	sharedstate->cache_entries_nr--;
+	sharedstate->cache_entries_cnt--;
 }
 
 bool cache_add(sharedstate_t *sharedstate, sharedstate_pkt_t *pkt)
 {
-	if (sharedstate->cache_entries_nr == CACHE_SIZE)
+	if (sharedstate->cache_entries_cnt == CACHE_SIZE)
 	{
 		return false;
 	}
-	int i = 0;
-	while (!sharedstate->cache_occupied_index[i++])
-		;
-	sharedstate->cache[i] = *pkt;
-	sharedstate->cache_occupied_index[i] = true;
-	sharedstate->cache_entries_nr++;
-
-	return true;
+	for (int i = 0; i < CACHE_SIZE; i++) {
+		if (!sharedstate->cache_occupied_index[i]) {
+			sharedstate->cache[i] = *pkt;
+			sharedstate->cache_occupied_index[i] = true;
+			sharedstate->cache_entries_cnt++;
+			return true;
+		}
+	}
+	LOG_INFO("Error: no place in cache, but there should be\n");
+	return false;
 }
 
 void cache_get_random_entries(sharedstate_t *sharedstate, uint8_t entry_nr,
-							  int entries[entry_nr])
+							  int *entries)
 {
-	if (sharedstate->cache_entries_nr < entry_nr)
+	if (sharedstate->cache_entries_cnt < entry_nr)
 	{
 		LOG_WARN("Not enough cache entries to get %u random ones\n", entry_nr);
 		return;
@@ -102,14 +94,16 @@ void cache_get_random_entries(sharedstate_t *sharedstate, uint8_t entry_nr,
 		}
 	}
 	// random shuffle and get entry_nr first elements
-	for (int i = sharedstate->cache_entries_nr - 1; i > 0; i--)
+	for (int i = entry_nr - 1; i > 0; i--)
 	{
 		int j = random_rand() % (i + 1);
 		int temp = occupied_indices[i];
 		occupied_indices[i] = occupied_indices[j];
 		occupied_indices[j] = temp;
 	}
-	memcpy(entries, occupied_indices, entry_nr);
+	for (int i = 0; i < entry_nr; i++) {
+		entries[i] = occupied_indices[i];
+	}
 }
 
 static void recv_callback(const void *data,
@@ -135,10 +129,9 @@ void init_sharedstate(sharedstate_t *sharedstate, int node_id)
 {
 	memset(sharedstate, 0, sizeof(sharedstate_t));
 
-	sharedstate->msgs_rx_nr = 0;
-	sharedstate->cache_entries_nr = 0;
+	sharedstate->input_buf_cnt = 0;
+	sharedstate->cache_entries_cnt = 0;
 	sharedstate->node_id = node_id;
-	sharedstate->tx_local = false;
 
 	sharedstate->msgs_dropped_nr = 0;
 	sharedstate->overload_cumulative = 0;
@@ -152,7 +145,6 @@ void init_sharedstate(sharedstate_t *sharedstate, int node_id)
 	nullnet_set_input_callback(recv_callback);
 	NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, 18);
 	nullnet_buf = (uint8_t *)&(sharedstate->output_buf[0]);
-	// nullnet_buf = (uint8_t *)&(sharedstate->local_entry);
 	nullnet_len = sizeof(sharedstate_pkt_t);
 	// nullnet_len = sizeof(sharedstate_pkt_t) * OUTPUT_BUF_SIZE;
 	LOG_INFO("SharedState initialized\n");
@@ -160,7 +152,7 @@ void init_sharedstate(sharedstate_t *sharedstate, int node_id)
 
 void sharedstate_rx(sharedstate_t *sharedstate, sharedstate_pkt_t *pkt)
 {
-	if (sharedstate->msgs_rx_nr == INPUT_BUF_SIZE)
+	if (sharedstate->input_buf_cnt == INPUT_BUF_SIZE)
 	{
 		LOG_INFO("Input buffer full, dropping pkt id: %u data: %u\n",
 				 pkt->pkt_id[1],
@@ -185,8 +177,8 @@ void sharedstate_rx(sharedstate_t *sharedstate, sharedstate_pkt_t *pkt)
 		LOG_INFO("Adding pkt id: %u data: %u to input buffer\n",
 				 pkt->pkt_id[1],
 				 pkt->data[0]);
-		sharedstate->input_buf[sharedstate->msgs_rx_nr] = *pkt;
-		sharedstate->msgs_rx_nr++;
+		sharedstate->input_buf[sharedstate->input_buf_cnt] = *pkt;
+		sharedstate->input_buf_cnt++;
 	}
 }
 
@@ -197,22 +189,18 @@ void sharedstate_app_send(sharedstate_t *sharedstate, void *data, uint16_t len)
 		LOG_INFO("Sent more than max data len: %u\n", PKT_DATA_SIZE_BYTES);
 		return;
 	}
-
-	sharedstate->local_entry.overload = 0;
-	sharedstate->local_entry.tstamp = RTIMER_NOW();
-	sharedstate->local_entry.pkt_id[0] = sharedstate->node_id;
-	sharedstate->local_entry.pkt_id[1] = msg_id++;
-	memcpy(sharedstate->local_entry.data, data, len);
-	sharedstate->tx_local = true;
-
-	LOG_INFO("App send id: %u data: %u\n",
-			 sharedstate->local_entry.pkt_id[1],
-			 sharedstate->local_entry.data[0]);
+	sharedstate_pkt_t pkt;
+	pkt.tstamp = RTIMER_NOW();
+	pkt.pkt_id[0] = sharedstate->node_id;
+	pkt.pkt_id[1] = msg_id++;
+	pkt.overload = 0;
+	memcpy(pkt.data, data, len);
+	cache_add(sharedstate, &pkt);
 }
 
 void sharedstate_update_cache(sharedstate_t *sharedstate)
 {
-	for (int i = 0; i < sharedstate->msgs_rx_nr; i++)
+	for (int i = 0; i < sharedstate->input_buf_cnt; i++)
 	{
 		int j = cache_contains(sharedstate, &(sharedstate->input_buf[i]));
 		if (j >= 0)
@@ -223,10 +211,10 @@ void sharedstate_update_cache(sharedstate_t *sharedstate)
 			sharedstate->input_buf[i].pkt_id[0] = 0;
 		}
 	}
-	if (CACHE_SIZE - sharedstate->cache_entries_nr < sharedstate->msgs_rx_nr)
+	if (CACHE_SIZE - sharedstate->cache_entries_cnt < sharedstate->input_buf_cnt)
 	{
-		int entries_needed = sharedstate->msgs_rx_nr -
-							 (CACHE_SIZE - sharedstate->cache_entries_nr);
+		int entries_needed = sharedstate->input_buf_cnt -
+							 (CACHE_SIZE - sharedstate->cache_entries_cnt);
 		int random_entries_indices[INPUT_BUF_SIZE];
 		cache_get_random_entries(sharedstate, entries_needed, random_entries_indices);
 		for (int i = 0; i < entries_needed; i++)
@@ -234,7 +222,7 @@ void sharedstate_update_cache(sharedstate_t *sharedstate)
 			cache_remove(sharedstate, random_entries_indices[i]);
 		}
 	}
-	for (int i = 0; i < sharedstate->msgs_rx_nr; i++)
+	for (int i = 0; i < sharedstate->input_buf_cnt; i++)
 	{
 		// if we didn't have this input buf entry in the cache
 		if (sharedstate->input_buf[i].pkt_id[0] != 0)
@@ -242,46 +230,23 @@ void sharedstate_update_cache(sharedstate_t *sharedstate)
 			cache_add(sharedstate, &sharedstate->input_buf[i]);
 		}
 	}
-	sharedstate->msgs_rx_nr = 0;
+	sharedstate->input_buf_cnt = 0;
 }
 
 void sharedstate_tx(sharedstate_t *sharedstate)
 {
 	sharedstate_update_cache(sharedstate);
-
-	int i = 0;
-	if (sharedstate->tx_local)
-	{
-		LOG_INFO("Tx local entry id: %u data: %u\n",
-				 sharedstate->local_entry.pkt_id[1],
-				 sharedstate->local_entry.data[0]);
-		sharedstate->tx_local = false;
-		sharedstate->output_buf[0] = sharedstate->local_entry;
-		i++;
-	}
-	if (sharedstate->cache_entries_nr < OUTPUT_BUF_SIZE - 1)
-	{
-		for (; i < sharedstate->cache_entries_nr; i++)
-		{
-			sharedstate->output_buf[i] = sharedstate->cache[i];
-		}
-
-		LOG_INFO("Broadcasting id: %u data: %u\n",
-				 sharedstate->output_buf[0].pkt_id[1],
-				 sharedstate->output_buf[0].data[0]);
-
-		sharedstate->msgs_dropped_nr = 0;
-		sharedstate->overload_cumulative = 0;
-
-		NETSTACK_NETWORK.output(NULL);
+	if (sharedstate->cache_entries_cnt < OUTPUT_BUF_SIZE) {
+		LOG_INFO("Not enough entries to tx\n");
 		return;
 	}
 
 	int entries[OUTPUT_BUF_SIZE];
-	cache_get_random_entries(sharedstate, OUTPUT_BUF_SIZE - i, entries);
-	for (; i < OUTPUT_BUF_SIZE; i++)
+	cache_get_random_entries(sharedstate, OUTPUT_BUF_SIZE, entries);
+	for (int i = 0; i < OUTPUT_BUF_SIZE; i++)
 	{
 		sharedstate->output_buf[i] = sharedstate->cache[entries[i]];
+		cache_remove(sharedstate, i);
 	}
 
 	LOG_INFO("Broadcasting id: %u data: %u\n",
@@ -323,3 +288,4 @@ PROCESS_THREAD(sharedstate_process, ev, data)
 
 	PROCESS_END();
 }
+
--- examples/sharedstate/sharedstate.h
+++ examples/sharedstate/sharedstate.h
@@ -36,17 +36,15 @@ typedef struct
 	sharedstate_pkt_t input_buf[INPUT_BUF_SIZE];
 	sharedstate_pkt_t output_buf[OUTPUT_BUF_SIZE];
 	sharedstate_pkt_t cache[CACHE_SIZE];
-
 	// either that shi(workaround) or hashmap
 	bool cache_occupied_index[CACHE_SIZE];
-	uint8_t msgs_rx_nr;
+
+	uint8_t input_buf_cnt;
+	uint8_t cache_entries_cnt;
+
 	uint8_t msgs_dropped_nr;
-	uint8_t cache_entries_nr;
 	uint16_t overload_cumulative;
 	int node_id;
-
-	sharedstate_pkt_t local_entry;
-	bool tx_local;
 } sharedstate_t;
 
 void init_sharedstate(sharedstate_t *sharedstate, int node_id);
@@ -54,7 +52,9 @@ void sharedstate_rx(sharedstate_t *sharedstate, sharedstate_pkt_t *pkt);
 void sharedstate_tx(sharedstate_t *sharedstate);
 void sharedstate_app_send(sharedstate_t *sharedstate, void *data, uint16_t len);
 
-inline int get_pkt_id(uint8_t pkt_id[PKT_ID_SIZE_BYTES]);
-// inline int get_pkt_tstamp(uint8_t tstamp[TIMESTAMP_SIZE_BYTES]);
+static inline int get_pkt_id(uint8_t pkt_id[PKT_ID_SIZE_BYTES])
+{
+	return pkt_id[1] << 8 | pkt_id[0];
+}
 
 #endif /* PROJECT_CONF_H_ */
